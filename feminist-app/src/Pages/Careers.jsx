d64d2bc3b7f075ee9ec4f121c8d343884002c515
eshihanda <eshihandamonn@gmail.com>
2026-01-18 23:30:55 +0300

    developed the careers page with vacancies

--- feminist-app/src/Pages/Careers.jsx
+++ feminist-app/src/Pages/Careers.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import '../Styles/Careers.css'
+import career from '../assets/career.png'
+import Footer from '../Components/Footer'
 
 const Careers = () => {
   return (
-    <div>Careers</div>
+    <>
+    <section className='d-flex align-items-center justify-content-center about-hero-section'
+        style={{backgroundImage:`url(${career})`, height:'60vh', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+        <div className='w-100 h-100 d-flex flex-column justify-content-center align-items-center'
+            style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
+            <h1 className='display-3 fw-bold text-white'> Career Opportunities</h1>
+            <p className="lead text-light mt-2">Join the Movement for Justice and Dignity</p>
+        </div>
+    </section>
+    <section className='careers-intro'>
+      <div className='container'>
+        {/* <p>We are always looking for passionate, committed individuals to join our
+          mission of advancing justice, dignity, and safety for girls and women in
+          Kibra.
+      </p> */}
+      <p>Feminist for Peace Rights and Justice Center is seeking passionate, values-driven
+      leaders to serve on our Board of Directors and support our mission to protect girls
+      and women from violence and strengthen grassroots feminist leadership.
+      </p>
+      </div>
+  </section>
+  <section className="vacancies-section">
+  <div className="container">
+    <h3 className="vacancies-title">Open Board Leadership Opportunities</h3>
+
+    <div className="vacancies-grid">
+
+      <div className="vacancy-card">
+        <h4>Board Member - Partnership and Development</h4>
+        <p className="vacancy-meta">Volunteer | Nairobi / Remote</p>
+
+        <a
+          href="public/jobs/Bdpartnership.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-golden"
+        >
+          View Role Description(PDF)
+        </a>
+      </div>
+
+      <div className="vacancy-card">
+        <h4>Board Member - Resource Mobilization</h4>
+        <p className="vacancy-meta">Volunteer | Nairobi / Remote</p>
+
+        <a
+          href="public/jobs/Bdresource.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-golden"
+        >
+          View Role Description(PDF)
+        </a>
+      </div>
+      <div className="vacancy-card">
+        <h4>Board Member - Feminist Leadership and Governance</h4>
+        <p className="vacancy-meta">Volunteer | Nairobi / Remote</p>
+
+        <a
+          href="public/jobs/Bdleadership.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-golden"
+        >
+          View Role Description(PDF)
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+<Footer />
+</>
   )
 }
 export default Careers;
+
